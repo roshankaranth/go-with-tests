1f0b08841c77b4385d584fe05a3ceeb95d8280ea
roshankaranth <roshankaranth@gmail.com>
2024-12-18 17:38:41 +0530

    hello world

---
+++ my-project/hello.go
@@ -0,0 +1,35 @@
+package main
+
+const (
+	spanish = "Spanish"
+	french  = "French"
+	hindi   = "Hindi"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+	hindiHelloPrefix   = "Namaste, "
+)
+
+func Hello(lang, name string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(lang) + name
+}
+
+func greetingPrefix(lang string) (prefix string) {
+	switch lang {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	case hindi:
+		prefix = hindiHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
+}

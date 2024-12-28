954e911ffca1b1eb5f227d2c9ebe630409572fca
roshankaranth <roshankaranth@gmail.com>
2024-12-28 19:41:08 +0530

    dependency injection

---
+++ dependencyinjection/greet.go
@@ -0,0 +1,30 @@
+package dependencyinjection
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func Greet(writer io.Writer, name string) {
+	//fmt.Printf("Hello, %s", name) this code writes the name to stdout, we need it to print to buffer
+	fmt.Fprintf(writer, "Hello, %s", name)
+
+}
+
+//write is a great general purpose interface for "put this data somewhere"
+//printf is very similar to Fprint, the difference is printf defaults to stdout wheras Fprintf
+//takes a witer to send the string to.
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	//this is an http handler
+	Greet(w, "world")
+}
+
+func main() {
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
+}
+
+//in this example the output stream was the dependency, where the greet function could only greet to stdout
+// we decided to inject dependecy, throght parameters into the function.
---
+++ dependencyinjection/greet_test.go
@@ -0,0 +1,20 @@
+package dependencyinjection
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	//Buffer type which is a struct, implements the Writer interface becasue it has the Write
+	//method
+	Greet(&buffer, "Chris")
+
+	got := buffer.String()
+	want := "Hello, Chris"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
